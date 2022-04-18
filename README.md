c4d251f4c32d42f85fcd1465d7e1a04c8f4f0af7
OliverrrrrrrLiu <zhuoran.o.liu@gmail.com>
2022-04-18 15:27:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Search with You.com
+A Simple Extension for VSCode to search easily with you.com! This extension is adapted from [Kamesh Kotwani](https://github.com/kameshkotwani/google-search).
+
+## How it works!
+Simply select the word or function you want to search, then right click and click on `Google this` in drop-down menu.
+
+# See it in Action
+#### when you select the text and right click you'll see `Search with you.com` in your drop-down menu
+
+![drop-down-menu](https://github.com/Su-Sea/vscode-youdotcom-extension/blob/main/resources/dropdown.png?raw=true)
+
+## How it opens in browser
+
+![browser-query-results](https://github.com/Su-Sea/vscode-youdotcom-extension/blob/main/resources/query.png?raw=true)
---
+++ extension.js
@@ -0,0 +1,54 @@
+//Strings
+const CFG_SECTION = "youdotcomSearch";
+const CFG_QUERY = "QueryTemplate";
+const CMD_ID = "extension.youdotcomSearch";
+
+const vscode = require("vscode");
+
+//Activating Function
+function activate(context) {
+  const disposable = vscode.commands.registerTextEditorCommand(
+    CMD_ID,
+    webSearch
+  );
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+//Empty Deactivate Function
+function deactivate() {}
+exports.deactivate = deactivate;
+
+//Function to launch the Search URL in default browser
+function webSearch() {
+  const selectedText = getSelectedText();
+  if (!selectedText) {
+    return;
+  }
+  const uriText = encodeURI(selectedText);
+  const youdotcomSearchCfg = vscode.workspace.getConfiguration(CFG_SECTION);
+  const queryTemplate = youdotcomSearchCfg.get(CFG_QUERY);
+  const query = queryTemplate.replace("%SELECTION%", uriText);
+  vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(query));
+}
+
+//getSelectedText creates a URL for search based on the selection
+function getSelectedText() {
+  const documentText = vscode.window.activeTextEditor.document.getText();
+  if (!documentText) {
+    return "";
+  }
+  const activeSelection = vscode.window.activeTextEditor.selection;
+  if (activeSelection.isEmpty) {
+    return "";
+  }
+  const selStartoffset = vscode.window.activeTextEditor.document.offsetAt(
+    activeSelection.start
+  );
+  const selEndOffset = vscode.window.activeTextEditor.document.offsetAt(
+    activeSelection.end
+  );
+
+  let selectedText = documentText.slice(selStartoffset, selEndOffset).trim();
+  return selectedText.replace(/\s\s+/g, " ");
+}
